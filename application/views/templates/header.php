b5a5c5ccd6ef0e7e1dc8b812b53a252c94a1307a
Choirulriz <achoirul.rizal@gmail.com>
2019-04-07 13:46:01 +0700

    halaman depan

---
+++ application/views/templates/header.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title><?= $judul; ?></title>
+</head>
+
+<body>
+
+    <nav class="navbar" style="background-color: #ffffff;">
+        <div class="row">
+            <div class="col">
+                <a class="navbar-brand" href="#">
+                    <img src="assets/img/LogoHandmadenesiaEarthHour.png" width="auto" height="60px" alt="Logo hand madenesia earth hour">
+                </a>
+            </div>
+            <div>
+                <input type="text" class="form-control form-rounded" placeholder="text">
+            </div>
+        </div>
+
+
+
+
+    </nav>
